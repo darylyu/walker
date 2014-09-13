3b80321530f9049a7bf155ef46919f1babd26363
D. Yu <dyu+honeypot@fastmail.fm>
2014-09-13 06:22:48 +0000

    main: change references to repos to targets

--- walker/main.py
+++ walker/main.py
@@ -4,27 +4,28 @@ import os
 import subprocess
 import sys
 
-def execute_command(repo):
-    os.chdir(repo)
+def execute_command(target):
+    os.chdir(target)
     command = sys.argv[1:]
     try:
         subprocess.check_call(command)
     except Exception as e:
-        print "ERROR in %s: %s" % (repo, e)
+        print "ERROR in %s: %s" % (target, e)
 
 def main():
     base = os.getcwdu() + "/"
-    repositories = None
+    targets = None
 
     # Get immediate child directories.
     for root, dirs, files in os.walk('.'):
-        repositories = dirs
-        break
+        targets = dirs
+        break  # dirty hack so we only get the first level
 
     # Traverse through the directories.
-    for repo in sorted(repositories, key=lambda s: s.lower()):
-        repo_full_path = base + repo
-        execute_command(repo_full_path)
+    for target in sorted(targets, key=lambda s: s.lower()):
+        target_full_path = base + target
+        print "walker: in %s" % target_full_path
+        execute_command(target_full_path)
         os.chdir(base)
 
 if __name__ == '__main__':
